9bcc953aad368fed97b51baf83e2f2ecdfe75628
Yukioooh <maroan.akka@edu.univ-fcomte.fr>
2026-05-08 18:22:41 +0200

    BundleInfo file ( describe bundle file )

---
+++ SL_Asset_Extractor.UI/SL_Asset_Extractor.Core/Models/BundleInfo.cs
@@ -0,0 +1,28 @@
+﻿// Représente un fichier .bundle scanné 
+
+namespace SL_Asset_Extractor.Core.Models { 
+    public class BundleInfo 
+    {
+        // id unique en bdd 
+        public int Id { get; set; }
+        
+        //nom du bundle 
+        public required string FileName { get; set; }
+        
+        // chemin complet 
+        public required string FullPath { get; set; }
+        
+        // hash du bundle 
+        public required string Hash { get; set; }
+        
+        // taille du fichier 
+        public long FileSize { get; set; }
+
+        // date du scan 
+        public DateTime LastScanned { get; set; }
+
+        //nb d'asset scannés 
+        public int ExtractedAssetsCount { get; set; }
+
+    }
+}
